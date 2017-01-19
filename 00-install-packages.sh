f6711a6e9a3efe63899013f7e86a8ab9859428f0
AJ Bowen <aj@soulshake.net>
2017-01-19 13:07:32 -0600

    Add buncha gstreamer codecs for YT and other sources

--- 00-install-packages.sh
+++ 00-install-packages.sh
@@ -7,5 +7,8 @@ sudo apt-get install -y \
     git \
     pulseaudio \
     pulseaudio-module-zeroconf \
+    gstreamer1.0-plugins-good \
+    gstreamer1.0-plugins-bad \
+    gstreamer1.0-plugins-ugly \
     libffi-dev \
     libspotify-dev # for pyspotify
